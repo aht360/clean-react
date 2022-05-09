f0085a0db6d2e3ef90375b26025efaa9796d737f
augusto.terra <augusto.terra@ifood.com.br>
2022-05-09 10:21:58 -0300

    chore: updating webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,11 +39,14 @@ module.exports = {
     }]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: './public'
+    },
     historyApiFallback: true,
-    port: 3000,
-    hot: true
+    port: 8080
   },
   externals: {
     react: 'React',
