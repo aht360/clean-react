d39bd5f9cc435be8df4f3acb415098fa4ec68c23
augusto.terra <augusto.terra@ifood.com.br>
2022-05-10 19:45:20 -0300

    feat: ensure login will not render spinner and error on start

--- src/presentation/components/FormStatus/FormStatus.tsx
+++ src/presentation/components/FormStatus/FormStatus.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { FormContext } from "@/presentation/contexts";
+import React, { useContext } from "react";
 import { Spinner } from "..";
 import styles from "./form-status-styles.scss";
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(FormContext);
   return (
-    <div className={styles.errorWrap}>
-      <Spinner className={styles.spinner} />
-      <span className={styles.error}>Erro</span>
+    <div data-testid="error-wrap" className={styles.errorWrap}>
+      {isLoading && <Spinner className={styles.spinner} />}
+      {!!errorMessage && <span className={styles.error}>{errorMessage}</span>}
     </div>
   );
 };
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,26 +4,43 @@ import {
   Input,
   LoginHeader
 } from "@/presentation/components";
-import React from "react";
+import { FormContext } from "@/presentation/contexts";
+import React, { useState } from "react";
 import styles from "./login-styles.scss";
 
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+};
+
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ""
+  });
+
   return (
     <div className={styles.login}>
       <LoginHeader />
-      <form className={styles.form}>
-        <h2>Login</h2>
+      <FormContext.Provider value={state}>
+        <form className={styles.form}>
+          <h2>Login</h2>
 
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
 
-        <button type="submit" className={styles.submit}>
-          Entrar
-        </button>
+          <button type="submit" className={styles.submit}>
+            Entrar
+          </button>
 
-        <span className={styles.link}>Criar conta</span>
-        <FormStatus />
-      </form>
+          <span className={styles.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </FormContext.Provider>
       <Footer />
     </div>
   );
