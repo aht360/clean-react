a25add2ab10b0e9b964d07177157225ae16c071b
augusto.terra <augusto.terra@ifood.com.br>
2022-05-10 19:45:59 -0300

    test: ensure login will not render spinner and error on start

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,7 +3,9 @@ import { render } from "@testing-library/react";
 import Login from "./Login";
 
 describe("Login page", () => {
-  it("", () => {
-    render(<Login />);
+  it("Should not render spinner and error on start", () => {
+    const { getByTestId } = render(<Login />);
+    const errorWrap = getByTestId("error-wrap");
+    expect(errorWrap.childElementCount).toBe(0);
   });
 });
