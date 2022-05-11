0bd20e80c70eb32a6e87f350ce0fce3c4e5142a4
augusto.terra <augusto.terra@ifood.com.br>
2022-05-11 14:57:06 -0300

    feat: ensure login present valid state if passowrd is valid

--- src/presentation/components/Input/Input.tsx
+++ src/presentation/components/Input/Input.tsx
@@ -24,11 +24,11 @@ const Input: React.FC<Props> = (props: Props) => {
   };
 
   const getStatus = (): string => {
-    return "🔴";
+    return error ? "🔴" : "🟢";
   };
 
   const getTitle = (): string => {
-    return error;
+    return error || "Ok";
   };
 
   return (
