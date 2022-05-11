db1c659705e2c7d794a5e30da697aacf4647073f
augusto.terra <augusto.terra@ifood.com.br>
2022-05-11 16:22:01 -0300

    feat: ensure login calls authentication with correct values

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,3 +1,4 @@
+import { Authentication } from "@/domain/usecases";
 import {
   Footer,
   FormStatus,
@@ -11,9 +12,10 @@ import styles from "./login-styles.scss";
 
 type Props = {
   validation?: Validation;
+  authentication?: Authentication;
 };
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: "",
@@ -31,11 +33,17 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     });
   }, [state.email, state.password]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault();
     setState({
       ...state,
       isLoading: true,
+    });
+    await authentication.auth({
+      email: state.email,
+      password: state.password,
     });
   };
 
