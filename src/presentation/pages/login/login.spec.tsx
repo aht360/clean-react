e748ead5f266350b37047a60f1ceb41a3030840c
augusto.terra <augusto.terra@ifood.com.br>
2022-05-11 14:35:24 -0300

    refactor: move ValidationSpy to its own file

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,23 +7,12 @@ import {
 } from "@testing-library/react";
 import Login from "./Login";
 import { Validation } from "@/presentation/protocols/validation";
+import { ValidationSpy } from "@/presentation/test";
 
 type SutTypes = {
   sut: RenderResult;
   validationSpy: Validation;
 };
-
-class ValidationSpy implements Validation {
-  errorMessage: string;
-  fieldName: string;
-  fieldValue: string;
-
-  validate(fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName;
-    this.fieldValue = fieldValue;
-    return this.errorMessage;
-  }
-}
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy();
