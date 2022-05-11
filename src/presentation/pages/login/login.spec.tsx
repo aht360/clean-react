9b57eff0aa978ca6abbd64ac5555eb7c5ffd24fc
augusto.terra <augusto.terra@ifood.com.br>
2022-05-11 12:53:03 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -64,4 +64,15 @@ describe("Login page", () => {
       email: "any_email",
     });
   });
+
+  it("Should call Validation with correct password", () => {
+    const { sut, validationSpy } = makeSut();
+
+    const passwordInput = sut.getByTestId("password");
+    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+
+    expect(validationSpy.input).toEqual({
+      password: "any_password",
+    });
+  });
 });
