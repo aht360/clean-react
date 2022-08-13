f91d0c634fa8c4f2f9db457d714405c20e55ee3b
augusto.terra <augusto.terra@ifood.com.br>
2022-08-13 15:51:41 -0300

    feat: ensure ValidationBuilder has email helper

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,4 +1,5 @@
 import { FieldValidation } from "@/validation/protocols/field-validation";
+import { EmailValidation } from "../email/email-validation";
 import { RequiredFieldValidation } from "../required-field/required-field-validation";
 
 export class ValidationBuilder {
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
     required (): ValidationBuilder {
         this.validations.push(new RequiredFieldValidation(this.fieldName))
+        return this
+    }
+
+    email (): ValidationBuilder {
+        this.validations.push(new EmailValidation(this.fieldName))
         return this
     }
 
