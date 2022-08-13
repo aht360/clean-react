6218422db66259bba82ff21a6a86f398c261acc4
augusto.terra <augusto.terra@ifood.com.br>
2022-08-13 15:26:08 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,18 +1,32 @@
 import { FieldValidationSpy } from "../test/mock-field-validation"
 import { ValidationComposite } from "./validation-composite"
 
+type SutTypes = {
+    sut: ValidationComposite,
+    fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+    const fieldValidationsSpy = [
+        new FieldValidationSpy('any_field'),
+        new FieldValidationSpy('any_field')
+    ]
+
+    const sut = new ValidationComposite(fieldValidationsSpy)
+
+    return {
+        sut,
+        fieldValidationsSpy
+    }
+}
+
 describe('ValidationComposite', () => {
     test('Should return error if any validation fails', () => {
-        const fieldValidationSpy = new FieldValidationSpy('any_field')
-        fieldValidationSpy.error = new Error('first_error_message');
+        const { sut, fieldValidationsSpy } = makeSut()
 
-        const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-        fieldValidationSpy2.error = new Error('any_error_message')
+        fieldValidationsSpy[0].error = new Error('first_error_message');
 
-        const sut = new ValidationComposite([
-            fieldValidationSpy,
-            fieldValidationSpy2
-        ])
+        fieldValidationsSpy[1].error = new Error('any_error_message');
 
         const error = sut.validate('any_field', 'any_value')
 
