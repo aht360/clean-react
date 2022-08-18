e965b02451cd6303afaae036dba203f45268db6e
augusto.terra <augusto.terra@ifood.com.br>
2022-08-17 23:22:56 -0300

    fix: change eslint version

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -1,13 +1,13 @@
-import { InvalidFieldError, RequiredFieldError } from "../../errors";
-import { FieldValidation } from "../../protocols/field-validation";
+import { InvalidFieldError, RequiredFieldError } from "../../errors"
+import { FieldValidation } from "../../protocols/field-validation"
 
 export class CompareFieldsValidation implements FieldValidation {
-    constructor(
+    constructor (
         readonly field: string,
         private readonly valueToCompare: string,
     ) {}
 
-    validate(value: string): Error | null {
-        return value !== this.valueToCompare ? new InvalidFieldError() : null;
+    validate (value: string): Error | null {
+        return value !== this.valueToCompare ? new InvalidFieldError() : null
     }
 }
